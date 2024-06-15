bf3f1e203074f6965594219dccd3814364af0c1a
Ganesh Tiwari <ganesht049@gmail.com>
2024-06-15 23:49:05 +0530

    feat: added code that can start express server and take screenshot

--- src/utils/og-image/index.js
+++ src/utils/og-image/index.js
@@ -1,16 +1,33 @@
 import puppeteer from 'puppeteer';
+import express from 'express';
 
-const browser = await puppeteer.launch({
-  product: 'firefox'
-});
-const page = await browser.newPage();
-const current_dir = process.cwd();
+const takeScreenshot = async () => {
+  const browser = await puppeteer.launch({
+    product: 'firefox'
+  });
+  const page = await browser.newPage();
+  const current_dir = process.cwd();
 
-const og_image_file = `${current_dir}/content/index.html`;
+  const og_image_file = `${current_dir}/content/index.html`;
 
-console.log(`url ${og_image_file}`)
-await page.goto("http://127.0.0.1:5500/content/");
-await page.setViewport({width: 1200, height: 628});
-await page.screenshot({ path: 'screenshot.jpg' });
+  console.log(`url ${og_image_file}`)
+  await page.goto("http://127.0.0.1:3000/");
+  await page.setViewport({ width: 1200, height: 628 });
+  await page.screenshot({ path: 'screenshot.jpg' });
 
-await browser.close();
+  await browser.close();
+};
+
+const startExpress = async () => {
+  const app = express()
+  const port = 3000
+
+  app.use('/',express.static(process.cwd() + '/content/'))
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+};
+
+startExpress();
+takeScreenshot();

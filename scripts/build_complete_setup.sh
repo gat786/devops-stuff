9ed6d70c6c4f61d28c51b4dc623c0062b6d10065
Ganesh Tiwari <ganesht049@gmail.com>
2024-06-16 11:12:28 +0530

    feat: added code to template file and save index.html

---
+++ scripts/build_complete_setup.sh
@@ -0,0 +1,16 @@
+WORKDIR="$(pwd)"
+OG_IMAGE_DIR="$(pwd)/scripts/og-image"
+
+# npm run build # this causes a loop 
+
+vite build
+
+echo workdir $WORKDIR
+echo og-image-dir $OG_IMAGE_DIR
+
+mkdir $OG_IMAGE_DIR/build/
+cp -r build/ $OG_IMAGE_DIR/build/
+
+cd $OG_IMAGE_DIR
+npm install
+npm run generate
--- scripts/og-image/index.js
+++ scripts/og-image/index.js
@@ -1,7 +1,13 @@
 import puppeteer from 'puppeteer';
 import express from 'express';
+import { glob } from 'glob';
+import fs from 'node:fs';
+import { JSDOM } from 'jsdom';
+import Handlebars from 'handlebars';
+import { options } from 'marked';
+import { title } from 'node:process';
 
-const takeScreenshot = async () => {
+const takeScreenshot = async ( path = null ) => {
   const browser = await puppeteer.launch({
     product: 'firefox'
   });
@@ -13,7 +19,10 @@ const takeScreenshot = async () => {
   console.log(`url ${og_image_file}`)
   await page.goto("http://127.0.0.1:3000/");
   await page.setViewport({ width: 1200, height: 628 });
-  await page.screenshot({ path: 'screenshot.jpg' });
+  
+  let ogImagePath = path != null ? path : 'screenshot.jpg'
+  await page.screenshot({ path: ogImagePath });
+  
 
   await browser.close();
 };
@@ -29,5 +38,47 @@ const startExpress = async () => {
   })
 };
 
-startExpress();
-takeScreenshot();
+const findHTMLFiles = async () => {
+
+  console.log(`current working directory: ${process.cwd()}`);
+  const htmlFiles = await glob(`${process.cwd()}/build/**/*.html`, { ignore: 'node_modules/**' })
+  
+  htmlFiles.forEach((file) => {
+    fs.readFile(file, 'utf-8',(err, data) => {
+      if(err){
+        console.error(err);
+        return;
+      }
+      const dom = new JSDOM(data);
+      const htmlDoc = dom?.window?.document;
+      let headElement = htmlDoc?.head;
+
+      const metaElements = headElement?.getElementsByTagName("meta");
+      for (let metaElementIndex = 0; metaElementIndex < metaElements.length; metaElementIndex++) {
+        const metaElement = metaElements.item(metaElementIndex);
+        let metaProperty = metaElement.getAttribute("property");
+        if (metaProperty == "og:title") {
+          let metaTitle = metaElement.getAttribute("content");
+          let contentFolder = `${process.cwd()}/content`;
+          const template_index = fs.readFileSync(
+            `${contentFolder}/template_index.html`,
+            { encoding: 'utf-8' }
+          );
+          const template  = Handlebars.compile(template_index);
+          const indexHtml = template({ title: metaTitle });
+
+          fs.writeFileSync(
+            `${contentFolder}/index.html`,
+            indexHtml
+          )
+
+          startExpress();
+          let path = `screenshot/${file}.jpg`
+          takeScreenshot(path);
+        }
+      }
+    })
+  })
+}
+
+findHTMLFiles()

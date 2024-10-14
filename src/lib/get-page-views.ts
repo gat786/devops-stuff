63475d80c2c897028a67d80968ac0da2a5b7a4bb
Ganesh Tiwari <ganesht049@gmail.com>
2024-10-14 23:59:41 +0530

    feat: adding correct json header

--- src/lib/get-page-views.ts
+++ src/lib/get-page-views.ts
@@ -1,5 +1,11 @@
 
 export const getPageViews = async (page_id: string) => {
-  let response = await fetch(`https://counter.gats.dev/get-views`, {method: 'POST', body: JSON.stringify({'page_id': page_id})});
+  let response = await fetch(`https://counter.gats.dev/get-views`, {
+    method: 'POST', 
+    body: JSON.stringify({'page_id': page_id}),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   return response.json();
 }

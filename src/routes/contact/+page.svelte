e6fa26628daacc19783039cb59dcbf4532fabb61
Ganesh Tiwari <ganesht049@gmail.com>
2024-11-21 00:10:30 +0530

    feat: added contact me page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,17 @@
 <script>
+  import Instagram from "$lib/icons/social-media/instagram.svelte";
+  import Linkedin from "$lib/icons/social-media/linkedin.svelte";
+  import XTwitter from "$lib/icons/social-media/x-twitter.svelte";
 
 </script>
+
+<div class="flex flex-col items-center gap-5 my-5">
+  <h1>Here's the different ways you could reach out to me</h1>
+  <div class="flex gap-8">
+    <a href="https://linkedin.com/in/gat786" class="underline flex items-center gap-4"> <Linkedin/> Linkedin</a>
+    <a href="https://x.com/igat786" class="underline flex items-center gap-4"> <XTwitter/> Twitter or X</a>
+    <a href="https://twitter.com/gats.dev" class="underline flex items-center gap-4"> <Instagram/> Instagram</a>
+  </div>
+
+  <div class="mt-8"/>
+</div>

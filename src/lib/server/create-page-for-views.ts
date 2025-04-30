ccab493a9429bb03a319e8681b560b9c0d72458b
Ganesh Tiwari <ganesht049@gmail.com>
2025-04-30 07:08:50 +0530

    fix: let to const

--- src/lib/server/create-page-for-views.ts
+++ src/lib/server/create-page-for-views.ts
@@ -1,22 +1,19 @@
-import {
-  HTTP_BASIC_AUTH_HEADER
-} from "$env/static/private"
+import { HTTP_BASIC_AUTH_HEADER } from "$env/static/private";
 
-const counter_app_base_url = "https://page-view-counter.apps.gats.dev"
+const counter_app_base_url = "https://page-view-counter.apps.gats.dev";
 
 export const setupPageViews = async (page_id: string) => {
-  console.log(HTTP_BASIC_AUTH_HEADER)
   let response = await fetch(`${counter_app_base_url}/create-page`, {
-    method: 'POST', 
-    body: JSON.stringify({page_id}),
+    method: "POST",
+    body: JSON.stringify({ page_id }),
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Basic ${HTTP_BASIC_AUTH_HEADER}`
-    }
+      "Content-Type": "application/json",
+      Authorization: `Basic ${HTTP_BASIC_AUTH_HEADER}`,
+    },
   });
 
   let jsonResponse = await response.json();
-  console.log('Response status: ', response.status);
-  console.log('Page views setup response: ', jsonResponse);
+  console.log("Response status: ", response.status);
+  console.log("Page views setup response: ", jsonResponse);
   return jsonResponse;
-}
+};
--- src/routes/blogs/[...slug]/+page.server.ts
+++ src/routes/blogs/[...slug]/+page.server.ts
@@ -1,60 +1,60 @@
 export const prerender = true;
 
-import matter from 'gray-matter';
-import { marked } from 'marked';
-import { blog_folder_prefix } from '$lib/constants'
-import type { Blog } from '$lib/models/blog';
+import matter from "gray-matter";
+import { marked } from "marked";
+import { blog_folder_prefix } from "$lib/constants";
+import type { Blog } from "$lib/models/blog";
 import { generateOgFile } from "$lib/og-generator";
 import { setupPageViews } from "$lib/server/create-page-for-views";
-
 
 export const load = async ({ params }) => {
   const path = params.slug;
 
-  let blog_file_path = [blog_folder_prefix, path].join('');
-  blog_file_path = blog_file_path + '.md';
-  console.log('Reading blog: ', blog_file_path);
+  let blog_file_path = [blog_folder_prefix, path].join("");
+  blog_file_path = blog_file_path + ".md";
+  console.log("Reading blog: ", blog_file_path);
 
   const file_matter = matter.read(blog_file_path);
 
-  const { title, description, created_on, tags, authors, posterImage } = file_matter.data;
+  const { title, description, created_on, tags, authors, posterImage } =
+    file_matter.data;
 
   const parsed_html = await marked.parse(file_matter.content);
 
-  let titleLower : string = title.toLowerCase()
-  let titleLowerWithoutSpace = titleLower.replaceAll(' ', '-');
-  let titleCleaned = titleLowerWithoutSpace.replaceAll(/[^0-9a-zA-Z_-]/g, '')
-  let ogFileName = `${titleCleaned}`;
-  let ogFilePath = `/images/ogImages/${ogFileName}`
+  const titleLower: string = title.toLowerCase();
+  const titleLowerWithoutSpace = titleLower.replaceAll(" ", "-");
+  const titleCleaned = titleLowerWithoutSpace.replaceAll(/[^0-9a-zA-Z_-]/g, "");
+  const ogFileName = `${titleCleaned}`;
+  const ogFilePath = `/images/ogImages/${ogFileName}`;
   await generateOgFile({
     ogContent: {
       title: title,
       publishedDate: (created_on as Date).toDateString(),
-      path: "Home > Blogs >"
+      path: "Home > Blogs >",
     },
     ogImageFileName: ogFileName,
-    pathToStoreImage: ogFilePath
+    pathToStoreImage: ogFilePath,
   });
 
   let page_id = `devops-stuff.dev/blogs/${path}`;
-  page_id = page_id.replaceAll("/","")
-  console.log('Setting up page views for: ', page_id);
+  page_id = page_id.replaceAll("/", "");
+  console.log("Setting up page views for: ", page_id);
 
   await setupPageViews(page_id);
 
-  let blog: Blog = {
-    front_matter: { 
-      title, 
-      description, 
+  const blog: Blog = {
+    front_matter: {
+      title,
+      description,
       created_on,
       posterImage,
-      ogFilePath: ogFilePath + '.jpg',
+      ogFilePath: ogFilePath + ".jpg",
       tags,
-      authors, 
+      authors,
       file_path: blog_file_path,
-      url_postfix: path
+      url_postfix: path,
     },
-    content: parsed_html
-  }
+    content: parsed_html,
+  };
   return blog;
-}
+};
